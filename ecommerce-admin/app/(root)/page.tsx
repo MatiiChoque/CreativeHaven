1ea5526f8a25f34af1f68e8aae22463a8e98f659
MatiiChoque <choquematia@gmail.com>
2023-08-03 20:44:39 -0300

    iniciando proyecto

---
+++ ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Panel Administrador</p>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
