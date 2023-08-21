94131f68a70000e9b1e3e6898744fd4ecc555842
MatiiChoque <choquematia@gmail.com>
2023-08-21 18:13:13 -0300

    update

---
+++ ecommerce-admin/components/store-switcher.tsx
@@ -0,0 +1,11 @@
+import { PopoverTrigger } from "@/components/ui/popover";
+
+type PopoverTriggerProps = React.ComponentPropsWithoutRef<
+  typeof PopoverTrigger
+>;
+
+interface StoreSwitcherProps extends PopoverTriggerProps {}
+
+export default function StoreSwitcher() {
+  return <div>Store Switcher</div>;
+}
